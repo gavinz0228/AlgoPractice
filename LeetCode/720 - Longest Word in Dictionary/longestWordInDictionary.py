156a5332578c54a504ca9738ae7bbdd8c9eef12e
gavin <gavinz0228@gmail.com>
2019-08-15 01:10:45 -0400

    720 - Longest Word in Dictionary

---
+++ LeetCode/720 - Longest Word in Dictionary/longestWordInDictionary.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestWord(self, words):
+        """
+        :type words: List[str]
+        :rtype: str
+        """
+        if not words:
+            return ""
+        wl = [(len(w), w)for w in words]
+        wl.sort()
+        ws = set( w[1] for w in wl if w[0] == 1)
+        res = [min(ws)]
+        #print(ws)
+        for i in range(len(ws) - 1, len(wl)):
+            newRes = []
+            if wl[i][1][:-1] in ws:
+                ws.add(wl[i][1])
+                if not res or len(wl[i][1]) > len(res[-1])  or wl[i][1] < res[-1]:
+                    res.append(wl[i][1])
+        if res:
+            return res[-1]
