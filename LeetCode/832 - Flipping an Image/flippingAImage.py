4565f66132edd648860755c08784c0e44c9e5c87
gavin <gavinz0228@gmail.com>
2019-08-16 19:15:12 -0400

    832 - Flipping an Image/flippingAImage.py

---
+++ LeetCode/832 - Flipping an Image/flippingAImage.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [  [x^1 for x in row[::-1]] for row in A]
