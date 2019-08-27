5be8009c1133ba6ddf8c8c4f1b0fbb9fbd25ee51
gavin <gavinz0228@gmail.com>
2019-08-26 22:19:34 -0400

    1161 - Maximum Level Sum of a Binary Tree

--- LeetCode/946 - Validate Stack Sequences/validateStackSequences.py
+++ LeetCode/946 - Validate Stack Sequences/validateStackSequences.py
@@ -6,26 +6,11 @@ class Solution(object):
         :rtype: bool
         """
         length = len(pushed)
-        stack = []
         popidx = 0
-        pushidx = 0
-        while popidx < length :
-            if stack and stack[-1] == popped[popidx]:
-                popidx += 1
-                stack.pop()
-                continue
-            found = False
-            for i in range(pushidx, length):
-                if pushed[i] != popped[popidx]:
-                    stack.append(pushed[i])
-                    pushidx += 1
-                else:
-                    found = True
-                    pushidx += 1
-                    popidx += 1
-                    break
-            if not found:
-                return False
-        return True
+        left, right = 0, 0
+        while popidx < length:
+            for  
+
+
                 
 print(Solution().validateStackSequences([1,0,2],[2,1,0]))
