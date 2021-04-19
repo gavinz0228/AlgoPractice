5152c46246941e5678a8a05fd05954061b6ac33a
Gavin <gavinz0228@gmail.com>
2021-04-19 00:52:24 -0400

    Create pairs.py

---
+++ HackerRank/Pairs/pairs.py
@@ -0,0 +1,48 @@
+"""
+Given an array of integers and a target value, determine the number of pairs of array elements that have a difference equal to the target value.
+
+Example
+
+
+There are three values that differ by : , , and . Return .
+
+Function Description
+
+Complete the pairs function below.
+
+pairs has the following parameter(s):
+
+int k: an integer, the target difference
+int arr[n]: an array of integers
+Returns
+
+int: the number of pairs that satisfy the criterion
+Input Format
+
+The first line contains two space-separated integers  and , the size of  and the target value.
+The second line contains  space-separated integers of the array .
+
+Constraints
+
+each integer  will be unique
+Sample Input
+
+STDIN       Function
+-----       --------
+5 2         arr[] size n = 5, k =2
+1 5 3 4 2   arr = [1, 5, 3, 4, 2]
+Sample Output
+
+3
+Explanation
+
+There are 3 pairs of integers in the set with a difference of 2: [5,3], [4,2] and [3,1]. .
+"""
+#to avoid repeatition, only check n + k
+def pairs(k, arr):
+    res = 0    
+    arr_set = set(arr)
+    for n in arr_set:
+        if n + k in arr_set:
+            res += 1
+    return res
