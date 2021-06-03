8ebb60717276342dcb4fb4af5948939c2280cfb8
Gavin <gavinz0228@gmail.com>
2021-06-02 23:50:29 -0400

    Create killProcess.py

---
+++ LeetCode/582 - Kill Process/killProcess.py
@@ -0,0 +1,70 @@
+'''
+582. Kill Process
+Medium
+
+683
+
+14
+
+Add to List
+
+Share
+You have n processes forming a rooted tree structure. You are given two integer arrays pid and ppid, where pid[i] is the ID of the ith process and ppid[i] is the ID of the ith process's parent process.
+
+Each process has only one parent process but may have multiple children processes. Only one process has ppid[i] = 0, which means this process has no parent process (the root of the tree).
+
+When a process is killed, all of its children processes will also be killed.
+
+Given an integer kill representing the ID of a process you want to kill, return a list of the IDs of the processes that will be killed. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+
+Input: pid = [1,3,10,5], ppid = [3,0,5,3], kill = 5
+Output: [5,10]
+Explanation: The processes colored in red are the processes that should be killed.
+Example 2:
+
+Input: pid = [1], ppid = [0], kill = 1
+Output: [1]
+ 
+
+Constraints:
+
+n == pid.length
+n == ppid.length
+1 <= n <= 5 * 104
+1 <= pid[i] <= 5 * 104
+0 <= ppid[i] <= 5 * 104
+Only one process has no parent.
+All the values of pid are unique.
+kill is guaranteed to be in pid.
+Accepted
+49,932
+Submissions
+77,682
+'''
+from collections import defaultdict
+class Solution(object):
+    def killProcess(self, pid, ppid, kill):
+        """
+        :type pid: List[int]
+        :type ppid: List[int]
+        :type kill: int
+        :rtype: List[int]
+        """
+        children = defaultdict(list)
+        killed = set()
+        for ppi in range(len(ppid)):
+            children[ppid[ppi]].append(pid[ppi])
+        killed.add(kill)
+        curr = children[kill]
+        while curr:
+            _curr = []
+            for c in curr:
+                killed.add(c)
+                _curr.extend(children[c])
+            curr = _curr
+        return killed
