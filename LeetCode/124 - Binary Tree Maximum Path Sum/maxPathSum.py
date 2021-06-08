7450186ef9caed385a6068d77c83ae99d517acf1
Gavin <gavinz0228@gmail.com>
2021-06-07 20:10:05 -0400

    Create maxPathSum.py

---
+++ LeetCode/124 - Binary Tree Maximum Path Sum/maxPathSum.py
@@ -0,0 +1,66 @@
+'''
+124. Binary Tree Maximum Path Sum
+Hard
+
+5921
+
+407
+
+Add to List
+
+Share
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+
+The path sum of a path is the sum of the node's values in the path.
+
+Given the root of a binary tree, return the maximum path sum of any path.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3]
+Output: 6
+Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+Example 2:
+
+
+Input: root = [-10,9,20,null,null,15,7]
+Output: 42
+Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 3 * 104].
+-1000 <= Node.val <= 1000
+Accepted
+519,849
+Submissions
+1,446,097
+'''
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxPathSum(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        curr_max = float('-inf')
+        def count(node):
+            if not node:
+                return 0
+            left = max(0, count(node.left))
+            right = max(0, count(node.right))
+            nonlocal curr_max
+            curr_max = max(curr_max, left + right + node.val)
+            
+            return max(left + node.val, right + node.val)
+        count(root)
+        return curr_max
