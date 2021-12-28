e5cf19fcc683755ed7ea908e3cf613a773fde708
Gavin <gavinz0228@gmail.com>
2021-12-27 23:23:32 -0500

    Create middleNode.py

---
+++ LeetCode/876 - Middle of the Linked List/middleNode.py
@@ -0,0 +1,61 @@
+"""
+876. Middle of the Linked List
+Easy
+
+4006
+
+103
+
+Add to List
+
+Share
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+Example 2:
+
+
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 100].
+1 <= Node.val <= 100
+Accepted
+482,926
+Submissions
+678,188
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return head
+        mid = head
+        curr = head
+        count = 0
+        while curr:
+            if count % 2 == 1:
+                mid = mid.next
+
+            curr = curr.next
+            count += 1
+        return mid
