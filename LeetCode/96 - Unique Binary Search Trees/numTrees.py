03421db130dcbf51c122c5dd01e935bac8fd557c
Gavin <gavinz0228@gmail.com>
2022-01-09 23:38:28 -0500

    Update numTrees.py

--- LeetCode/96 - Unique Binary Search Trees/numTrees.py
+++ LeetCode/96 - Unique Binary Search Trees/numTrees.py
@@ -28,15 +28,31 @@ Constraints:
 
 1 <= n <= 19
 """
+
+"""
+#recursion with memorization
 class Solution:
     def numTrees(self, n: int) -> int:
+        mem = {0:1, 1:1}
         def recursion(num):
+            if num in mem:
+                return mem[num]
             if num == 1 or num == 0:
                 return 1
             res = 0
             for i in range(num):
                 res += recursion(i) * recursion(num - i - 1)
-
+            mem[num] = res
             return res
         return recursion(n)
+            
+"""
+#dp derived from the above
+class Solution:
+    def numTrees(self, n: int) -> int:
+        dp = [1,1]
+        for i in range(2, n + 1):
+            dp.append(sum([ dp[j] * dp[i - j - 1] for j in range(i)]))
+        return dp[-1]
+
             
